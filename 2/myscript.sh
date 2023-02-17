0f1d1537ca8342a181b7c39505010588d1b75049
elicia4 <a@bo.ba>
2023-02-17 10:31:29 +0300

    initial commit

---
+++ 2/myscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# LINES STARTED WITH THE '#' SYMBOL ARE IGNORED
+# YOU CAN WRITE ANYTHING ON THESE LINES, THEY 
+# WILL BE IGNORED BY THE INTERPRETER
+#ls
+#pwd
+
+echo "Hello World!"
+echo "My current working directory is:"
+pwd
