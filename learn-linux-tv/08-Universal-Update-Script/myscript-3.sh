023b68afb21ddd23c7bee64fd437d653e5c5b262
elicia4 <a@bo.ba>
2023-04-08 21:42:01 +0300

    add #8

---
+++ learn-linux-tv/08-Universal-Update-Script/myscript-3.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# You can simplify the previous version of the script by using '||', which is
+# OR.
+
+release_file=/etc/os-release
+
+if grep -q "arch" $release_file
+then
+	sudo pacman -Syyu 
+fi
+
+# Check if it contains ubuntu OR debian
+if grep -q "ubuntu" $release_file || grep -q "debian" $release_file
+then
+	sudo apt update
+	sudo apt dist-upgrade
+fi
+
