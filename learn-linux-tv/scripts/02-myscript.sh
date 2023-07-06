7ce222489e3b91fd158e1801583267a0e7f77ceb
elicia4 <shvanea2000@gmail.com>
2023-07-06 13:50:58 +0300

    format 02

--- learn-linux-tv/scripts/02-myscript.sh
+++ learn-linux-tv/scripts/02-myscript.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
-# LINES STARTED WITH THE '#' SYMBOL ARE IGNORED
-# YOU CAN WRITE ANYTHING ON THESE LINES, THEY 
-# WILL BE IGNORED BY THE INTERPRETER
-#ls
-#pwd
+# Lines started with the '#' symbol are ignored, you can write anything on
+# these lines, they will be ignored by the interpreter, for example:
+# pwd
 
 echo "Hello World!"
 echo "My current working directory is:"
